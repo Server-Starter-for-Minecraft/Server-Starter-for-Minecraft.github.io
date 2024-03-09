4c51640c2b63952986bbd6a3d69ccbdd841cef84
CivilTT <civiltt.ss@gmail.com>
2024-03-10 00:29:16 +0900

    🍱 Added base components

---
+++ src/components/utils/base/ssSelect.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Prop {
+  options?: readonly any[];
+  label?: string;
+  disable?: boolean;
+  dense?: boolean;
+  optionLabel?: string;
+  optionValue?: string;
+}
+
+const prop = defineProps<Prop>();
+const model = defineModel();
+</script>
+
+<template>
+  <q-select
+    v-model="model"
+    filled
+    :options="options"
+    :label="label"
+    :dense="dense"
+    :popup-content-style="{ fontSize: '0.9rem' }"
+    :disable="disable"
+    emit-value
+    map-options
+    :option-label="optionLabel"
+    :option-value="optionValue"
+    style="font-size: 0.9rem"
+  />
+</template>
