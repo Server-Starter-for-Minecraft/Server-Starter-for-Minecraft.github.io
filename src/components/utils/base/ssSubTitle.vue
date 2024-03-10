f731413d6427c65109a82f5645c8da9d93f9b188
CivilTT <civiltt.ss@gmail.com>
2024-03-11 00:21:16 +0900

    ✨ Added title components

---
+++ src/components/utils/base/ssSubTitle.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+const isDark = () => $q.dark.isActive;
+</script>
+
+<template>
+  <h2 class="row items-center q-my-sm q-px-sm" :class="isDark() ? '' : 'marker'">
+    <svg class="icon" :class="isDark() ? 'fill-primary' : ''">
+      <use xlink:href="icons/systemLogo_filled.svg#filledLogo" />
+    </svg>
+    <span class="text">
+      <slot />
+    </span>
+  </h2>
+</template>
+
+<style scoped lang="scss">
+.marker {
+  background: linear-gradient(transparent 60%, $primary 0%);
+  width: fit-content;
+}
+
+.icon {
+  margin-right: 0.5rem;
+  width: 2rem;
+  height: 2rem;
+}
+
+.fill-primary {
+  fill: $primary;
+}
+
+.text {
+  font-family: 'Genokaku JP';
+  font-size: 1.2rem;
+  font-weight: 600;
+  letter-spacing: normal;
+  line-height: 1rem;
+}
+</style>
---
+++ src/components/utils/base/ssTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="row items-center q-my-md q-px-sm">
+    <q-img src="icons/systemLogo.svg" class="icon" />
+    <span class="text">
+      <slot />
+    </span>
+  </h1>
+</template>
+
+<style scoped lang="scss">
+.icon {
+  margin-right: 1rem;
+  width: 3.2rem;
+}
+
+.text {
+  font-family: 'Genokaku JP';
+  font-size: 1.8rem;
+  font-weight: 600;
+  letter-spacing: normal;
+}
+</style>
