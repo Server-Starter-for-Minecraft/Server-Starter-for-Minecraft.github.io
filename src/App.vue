a56e86faf0d3be58630e7db893a272981f6d8a5c
CivilTT <civiltt.ss@gmail.com>
2024-03-11 01:16:46 +0900

    🔥 Removed layout code

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const leftDrawerOpen = ref(false);
+import HeaderView from 'src/components/HeaderView.vue';
 </script>
 
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
-        <q-btn stretch flat to="/">
-          <q-toolbar-title>
-            <span class="title">
-              <b>Server Starter</b> for <b>Minecraft</b>
-            </span>
-          </q-toolbar-title>
-        </q-btn>
-      </q-toolbar>
-    </q-header>
-
-    <SideMenuView v-model="leftDrawerOpen" />
+    <HeaderView />
 
     <q-page-container>
-      <router-view></router-view>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
-<style scoped lang="scss">
-.title {
-  text-transform: none;
-  font-size: 1.2rem;
-  line-height: 1rem;
-  font-family: 'Quicksand';
-  font-weight: 400;
-
-  b {
-    font-size: 1.5rem;
-    font-family: 'PT Sans', sans-serif;
-    font-weight: 900;
-  }
-}
-</style>
