8d767011bf08a4a42af8a42ecac4f189e14fa0f2
CivilTT <civiltt.ss@gmail.com>
2024-03-11 00:56:56 +0900

    🔥 Removed old files and codes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,48 +7,6 @@ const routes: RouteRecordRaw[] = [
     component: () => import('pages/HomeView.vue'),
   },
   {
-    path: '/intro',
-    name: 'intro',
-    component: () => import('pages/IntroductionView.vue'),
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('pages/AboutView.vue'),
-  },
-  // {
-  //   path: '/main',
-  //   name: 'main',
-  //   component: () => import('pages/MainWindow.vue')
-  // },
-  // {
-  //   path: '/world',
-  //   name: 'world',
-  //   component: () => import('pages/WorldSettings.vue')
-  // },
-  // {
-  //   path: '/system',
-  //   name: 'system',
-  //   component: () => import('pages/SystemSettings.vue')
-  // },
-  {
-    path: '/funcs',
-    component: () => import('pages/FuncsView.vue'),
-  },
-  {
-    path: '/ShareWorld',
-    name: 'sw',
-    component: () => import('pages/ShareWorld.vue'),
-  },
-  {
-    path: '/PortMapping',
-    name: 'apm',
-    component: () => import('pages/AutoPortMapping.vue'),
-  },
-  {
     path: '/credit',
     name: 'credit',
     component: () => import('pages/CreditPage.vue'),
