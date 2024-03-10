e842a0a390dcff0dd2c29dd5305f889b2ec4f918
CivilTT <civiltt.ss@gmail.com>
2024-03-11 01:09:29 +0900

    💄 Added Header component and mainField css

---
+++ src/components/MainLayout/HeaderView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Page {
+  key: string
+  to: string
+}
+const pages: Page[] = [
+  {
+    key: 'intro',
+    to: 'intro'
+  }
+]
+</script>
+
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn stretch flat to="/">
+        <q-toolbar-title>
+          <span class="title">
+            <b>Server Starter</b> for <b>Minecraft</b>
+          </span>
+        </q-toolbar-title>
+      </q-btn>
+    </q-toolbar>
+  </q-header>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,13 @@
+<script setup lang="ts">
+import HeaderView from 'src/components/MainLayout/HeaderView.vue';
+</script>
+
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
-        <q-btn stretch flat to="/">
-          <q-toolbar-title>
-            <span class="title">
-              <b>Server Starter</b> for <b>Minecraft</b>
-            </span>
-          </q-toolbar-title>
-        </q-btn>
-      </q-toolbar>
-    </q-header>
-
-    <SideMenuView v-model="leftDrawerOpen" />
+    <HeaderView />
 
     <q-page-container>
-      <router-view></router-view>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import SideMenuView from 'src/pages/SideMenuView.vue';
-
-const leftDrawerOpen = ref(false);
-</script>
