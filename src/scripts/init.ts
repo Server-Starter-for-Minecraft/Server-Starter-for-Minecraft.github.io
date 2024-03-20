268825d132e5f7ace2bd2c74cdacdd5cb7c49c3a
CivilTT <civiltt.ss@gmail.com>
2024-03-20 14:45:06 +0900

    🚨 Fixed the lint error in `init.ts`

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,13 +1,13 @@
-import { useSystemStore } from "src/stores/SystemStore";
+import { useSystemStore } from 'src/stores/SystemStore';
 
 /** ホームページの起動時処理を記述 */
 export async function initProcess() {
-  getProductVersion()
+  getProductVersion();
 }
 
 /**
  * ServerStarter2の最新バージョンを取得する
- * 
+ *
  * 取得文字列は`v0.0.0`の形式
  */
 async function getProductVersion() {
@@ -15,7 +15,6 @@ async function getProductVersion() {
     'https://api.github.com/repos/CivilTT/ServerStarter2/releases/latest'
   );
 
-  const sysStore = useSystemStore()
+  const sysStore = useSystemStore();
   sysStore.latestProductVersion = (await jsonObj.json()).name;
 }
-
