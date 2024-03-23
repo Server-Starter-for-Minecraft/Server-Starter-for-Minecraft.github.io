ac798ceeaf09b39ee1e6ce3dbc19ff6c2f12965b
CivilTT <civiltt.ss@gmail.com>
2024-03-24 01:38:46 +0900

    💄 Added the basic design for the drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
 import { initProcess } from './scripts/init';
+import { Page } from './router/routes';
 import HeaderView from 'src/components/App/HeaderView.vue';
 import DrawerView from './components/App/DrawerView.vue';
 
 const $q = useQuasar();
+const pages: { key: Page; icon: string }[] = [
+  { key: 'intro', icon: 'flag' },
+  { key: 'features', icon: 'category' },
+  { key: 'q_a', icon: 'help' },
+  { key: 'terms', icon: 'gavel' },
+];
 
 initProcess(window.navigator.userAgent);
 
@@ -14,8 +21,8 @@ $q.dark.set('auto');
 
 <template>
   <q-layout view="lHh Lpr lFf">
-    <HeaderView />
-    <DrawerView />
+    <HeaderView :showing-pages="pages.map(val => val.key)" />
+    <DrawerView :showing-pages="pages" />
 
     <q-page-container>
       <router-view />
--- src/components/App/DrawerView.vue
+++ src/components/App/DrawerView.vue
@@ -1,18 +1,50 @@
 <script setup lang="ts">
+import { Page } from 'src/router/routes';
 import { useSystemStore } from 'src/stores/SystemStore';
+import ColorThemeBtn from './core/colorThemeBtn.vue';
+import LanguageSelecter from './core/languageSelecter.vue';
+
+interface Prop {
+  showingPages: { key: Page; icon: string }[];
+}
+defineProps<Prop>();
 
 const sysStore = useSystemStore();
 </script>
 
 <template>
-  <q-drawer
-    v-model="sysStore.drawer"
-    side="right"
-  >
-    <q-scroll-area class="fit">
-      <div class="q-pa-sm">
-        <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
+  <q-drawer v-model="sysStore.drawer" side="right">
+    <div class="vertical-scroll">
+      <div class="row items-center q-ma-md">
+        <q-img src="icons/systemLogo.svg" style="width: 1.5rem;" />
+        <q-space />
+        <q-btn icon="close" flat dense @click="sysStore.drawer = false" />
       </div>
-    </q-scroll-area>
+
+      <q-list padding class="full-width">
+        <template v-for="page in showingPages" :key="page.key">
+          <q-item clickable :to="page.key" style="font-size: 1rem">
+            <q-item-section avatar>
+              <q-icon :name="page.icon" size="1.5rem" />
+            </q-item-section>
+            <q-item-section>
+              {{ $t(`layout.header.pages.${page.key}`) }}
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-list>
+
+      <q-space />
+      <q-separator inset class="q-my-md" />
+
+      <div class="row items-center q-mx-md">
+        <span class="col">
+          カラーテーマ変更
+        </span>
+        <ColorThemeBtn class="col-1" />
+      </div>
+
+      <LanguageSelecter :dark="$q.dark.isActive" class="q-pa-md" />
+    </div>
   </q-drawer>
 </template>
