bf6bfe92a8e88d36766df68b7e6248d23158bdc5
CivilTT <civiltt.ss@gmail.com>
2024-03-24 02:15:41 +0900

    🩹 Fixed pathes for nested pages

--- src/pages/Intro/IntroPage.vue
+++ src/pages/Intro/IntroPage.vue
@@ -8,8 +8,8 @@ import SsTitle from 'src/components/utils/base/texts/ssTitle.vue';
   サブページへジャンプ
 
   <div class="q-butter-md column">
-    <q-btn color="info" label="install" to="install" />
-    <q-btn color="info" label="run-server" to="run-server" />
-    <q-btn color="info" label="join-server" to="join-server" />
+    <q-btn color="info" label="install" to="./intro/install" />
+    <q-btn color="info" label="run-server" to="/intro/run-server" />
+    <q-btn color="info" label="join-server" to="/intro/join-server" />
   </div>
 </template>
