f9050a25d0ed4416617a6d7cb9af265729a216d5
CivilTT <civiltt.ss@gmail.com>
2024-03-29 00:05:35 +0900

    💄 Set distance between each block

--- src/components/Pages/HOME/ProductIntro.vue
+++ src/components/Pages/HOME/ProductIntro.vue
@@ -5,19 +5,21 @@ import SsTitle from 'src/components/utils/base/texts/ssTitle.vue';
 </script>
 
 <template>
-  <SsTitle>Server Starter for Minecraft とは？</SsTitle>
-
-  <p class="q-pt-lg">
-    ServerStarter2（Server Starter for Minecraft）はマインクラフトのマルチプレイのためのソフトウェアです．
-  </p>
-  <p>
-    導入はとても簡単！<br>
-    <SsA url="/intro">ServerStarter2のはじめかた</SsA>を見れば誰でも利用することができます．
-  </p>
-  <p>
-    ワンクリックでマルチプレイ用のワールドを起動し，みんなでマインクラフトを楽しみましょう！！
-  </p>
-  <p>
-    ※本ソフトはMinecraft Java Edition（Java版）に限られます．Minecraft Bedrock Edition（統合版）には対応していません．
-  </p>
+  <div>
+    <SsTitle>Server Starter for Minecraft とは？</SsTitle>
+  
+    <p class="q-pt-lg">
+      ServerStarter2（Server Starter for Minecraft）はマインクラフトのマルチプレイのためのソフトウェアです．
+    </p>
+    <p>
+      導入はとても簡単！<br>
+      <SsA url="/intro">ServerStarter2のはじめかた</SsA>を見れば誰でも利用することができます．
+    </p>
+    <p>
+      ワンクリックでマルチプレイ用のワールドを起動し，みんなでマインクラフトを楽しみましょう！！
+    </p>
+    <p>
+      ※本ソフトはMinecraft Java Edition（Java版）に限られます．Minecraft Bedrock Edition（統合版）には対応していません．
+    </p>
+  </div>
 </template>
