566211ea797b83a04fe178217e70fe4781e9305b
CivilTT <civiltt.ss@gmail.com>
2024-03-31 13:20:51 +0900

    🩹 Fixed the code structure in TermsIntro.vue

--- src/pages/TermsPage.vue
+++ src/pages/TermsPage.vue
@@ -1,8 +1,14 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="mainField">
+    <!-- <q-btn
+      v-if="$route.path !== '/terms'"
+      dense
+      color="primary"
+      label="back"
+      @click="$router.go(-1)"
+    /> -->
     <RouterView />
   </div>
 </template>
