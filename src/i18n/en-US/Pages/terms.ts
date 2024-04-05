1c0eca2ca4ab81f99d5d6e84ed81df4c5006a707
CivilTT <civiltt.ss@gmail.com>
2024-04-06 03:36:36 +0900

    🌐 Added card texts in the terms page

--- src/i18n/en-US/Pages/terms.ts
+++ src/i18n/en-US/Pages/terms.ts
@@ -16,6 +16,20 @@ export const enUSTerms: MessageSchema['terms'] = {
         All users can use this software by agreeing to the terms of use',
       btn: 'Open the terms of use',
     },
+    privacy: {
+      title: 'Privacy policy',
+      desc: '\
+        We have summarized a consistent approach to user data in this project.\n\
+        Data submission is voluntary, and we will not force users to submit data in any way.',
+      btn: 'Open the privacy policy',
+    },
+    broadcast: {
+      title: 'Terms of broadcast',
+      desc: '\
+        This terms for users broadcast the products of this project including ServerStarter2, through the Internet and other media.\n\
+        We are welcome to positive sharing, so you have to read this terms before broadcasting your work!',
+      btn: 'Open the terms of broadcast',
+    },
     specs: {
       title: 'Technical specs',
       desc: '\
