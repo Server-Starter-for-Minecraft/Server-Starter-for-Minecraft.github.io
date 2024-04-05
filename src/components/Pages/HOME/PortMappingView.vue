e5ab458df15b3ffa834c172e421ecee7a795e36d
nozz-mat <minaharu318@gmail.com>
2024-04-05 23:24:03 +0900

    ✨ ポート開放とまとめ部分のコンポーネントを追加

---
+++ src/components/Pages/HOME/PortMappingView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SsLinkBtn from 'src/components/utils/base/btns/ssLinkBtn.vue';
+import SsRouterBtn from 'src/components/utils/base/btns/ssRouterBtn.vue';
+import SsStrong from 'src/components/utils/base/texts/ssStrong.vue';
+import ssSubTitle from 'src/components/utils/base/texts/ssSubTitle.vue';
+</script>
+
+<template>
+  <ssSubTitle>
+    ポート開放の設定も不要 !!
+  </ssSubTitle>
+  <p>
+    ServerStarterは面倒なポート開放の設定いらずで,マルチプレイができます.<br />
+    「ルーターの設定ができない」<br />
+    「 設定したのに参加できない」<br />
+    そんな悩みも<SsStrong>ポート開放不要化機能</SsStrong>で一気に解決 !!
+  </p>
+  <p>
+    Ngrok <SsLinkBtn url="https://ngrok.com"/>に無料で登録して,面倒な設定をすべてスキップしましょう!
+  </p>
+  <div class="flex justify-end">
+      <SsRouterBtn to="/intro">
+        ポート開放不要化について詳しく
+      </SsRouterBtn>
+    </div>
+</template>
