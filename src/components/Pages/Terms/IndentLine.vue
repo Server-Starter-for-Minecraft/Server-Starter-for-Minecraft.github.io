2055259bccbb876766975f90d6ca687dbbe9ab42
CivilTT <CivilTT@users.noreply.github.com>
2024-04-08 13:30:28 +0000

    🚨 Apply Prettier Change

--- src/components/Pages/Terms/IndentLine.vue
+++ src/components/Pages/Terms/IndentLine.vue
@@ -2,14 +2,14 @@
 import { defineProps } from 'vue';
 
 interface Prop {
-  title: string
+  title: string;
 }
-defineProps<Prop>()
+defineProps<Prop>();
 </script>
 
 <template>
   <div class="row q-gutter-xs">
-    <span class="col-1" style="min-width: 10rem;">{{ title }}</span>
-    <span class="col" style="min-width: 10rem;"><slot /></span>
+    <span class="col-1" style="min-width: 10rem">{{ title }}</span>
+    <span class="col" style="min-width: 10rem"><slot /></span>
   </div>
 </template>
