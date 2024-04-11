c348b1ba97203b5a25cee2679b8ddcc5ebfcc1fe
CivilTT <civiltt.ss@gmail.com>
2024-04-12 00:14:20 +0900

    💄 Fixed download btn's style for the light mode

--- src/components/Pages/HOME/DownloadView.vue
+++ src/components/Pages/HOME/DownloadView.vue
@@ -7,9 +7,9 @@ import { isLinux, isMac, isWindows } from 'src/scripts/checkDevice';
 
 <template>
   <div class="column q-gutter-md">
-    <download-btn v-if="isWindows()" :os-name="'windows'" />
-    <download-btn v-else-if="isMac()" :os-name="'mac'" />
-    <download-btn v-else-if="isLinux()" disable :os-name="'linux'" />
+    <download-btn v-if="isWindows()" dark :os-name="'windows'" />
+    <download-btn v-else-if="isMac()" dark :os-name="'mac'" />
+    <download-btn v-else-if="isLinux()" dark disable :os-name="'linux'" />
     <SsStrong v-else class="q-my-md text-desc" style="font-size: 1rem">
       本ソフトはPCにてご利用下さい！
     </SsStrong>
