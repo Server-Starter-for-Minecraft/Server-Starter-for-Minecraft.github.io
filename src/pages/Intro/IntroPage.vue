c17b7f289a8e6eaf16f4d5a7f5eb2b0bbda24cf8
CivilTT <civiltt.ss@gmail.com>
2024-04-12 00:16:54 +0900

    ✨ Added the first intro page

--- src/pages/Intro/IntroPage.vue
+++ src/pages/Intro/IntroPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import DownloadBtn from 'src/components/Pages/HOME/core/DownloadBtn.vue';
 import SsRouterBtn from 'src/components/utils/base/btns/ssRouterBtn.vue';
+import SsStrong from 'src/components/utils/base/texts/ssStrong.vue';
 import SsSubTitle from 'src/components/utils/base/texts/ssSubTitle.vue';
 import SsTitle from 'src/components/utils/base/texts/ssTitle.vue';
 </script>
@@ -8,9 +9,7 @@ import SsTitle from 'src/components/utils/base/texts/ssTitle.vue';
 <template>
   <div>
     <SsTitle>ServerStarter2のはじめかた</SsTitle>
-    <p>
-      ServerStarter2のインストールからサーバーの作成までを完全サポート！
-    </p>
+    <p>ServerStarter2のインストールからサーバーの作成までを完全サポート！</p>
     <p>
       やり方が分からない時やトラブルに遭遇した時には，いつでもこのホームページを閲覧してください！
     </p>
@@ -18,26 +17,34 @@ import SsTitle from 'src/components/utils/base/texts/ssTitle.vue';
 
   <div>
     <SsSubTitle>まずはインストール</SsSubTitle>
-    <p>
-      インストールボタンを押してインストーラーをダウンロードしましょう！
-    </p>
-    <p>
-      インストールがうまくいかない場合は「詳細を確認」ボタンを押して別ページをご確認ください
-    </p>
+    <p>インストールボタンを押してインストーラーをダウンロードしましょう！</p>
+    <p>うまくいかない場合は「インストールの詳細」をご確認ください</p>
     <div class="row q-gutter-md">
       <download-btn :os-name="'windows'" />
       <download-btn :os-name="'mac'" />
       <download-btn :os-name="'linux'" />
-      <SsRouterBtn to="/intro/install">詳細を確認</SsRouterBtn>
+      <SsRouterBtn to="/intro/install">インストールの詳細</SsRouterBtn>
     </div>
   </div>
 
+  <div>
+    <SsSubTitle>ワールドを起動</SsSubTitle>
+    <p>ServerStarter2を起動し，さっそくワールドを立ち上げましょう！</p>
+    <p>
+      基本の<SsStrong>バニラサーバー</SsStrong>から，
+      <SsStrong>MODサーバー</SsStrong>・
+      <SsStrong>配布ワールド</SsStrong>
+      といったちょっと変わったサーバーまで，様々なサーバーを起動することができます
+    </p>
+    <SsRouterBtn to="/intro/run-server">ワールド起動方法の解説</SsRouterBtn>
+  </div>
 
-  <!-- サブページへジャンプ
-
-  <div class="q-butter-md column">
-    <q-btn color="info" label="install" to="./intro/install" />
-    <q-btn color="info" label="run-server" to="/intro/run-server" />
-    <q-btn color="info" label="join-server" to="/intro/join-server" />
-  </div> -->
+  <div>
+    <SsSubTitle>起動したワールドに参加</SsSubTitle>
+    <p>ワールドが立ち上がったら，いよいよみんなで遊ぶ準備が整いました！</p>
+    <p>
+      必要な情報をプレイヤーに案内し，マルチプレイを楽しみましょう！
+    </p>
+    <SsRouterBtn to="/intro/join-server">ワールドへの参加方法</SsRouterBtn>
+  </div>
 </template>
