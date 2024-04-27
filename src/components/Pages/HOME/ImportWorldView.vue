5bd3c07dfbff200336591515b12e45bccd90ec39
nozz-mat <minaharu318@gmail.com>
2024-04-27 19:14:08 +0900

    ♻️ ImageBlockを使っていたコンポーネントをImgTxtBlockを使用して作成

---
+++ src/components/Pages/HOME/ImportWorldView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ImgTxtBlock from 'src/components/utils/blocks/ImgTxtBlock.vue';
+</script>
+
+<template>
+  <div class="q-pr-md">
+    <ImgTxtBlock
+        title="個人ワールドや配布マップもみんなで冒険 !!"
+        imgPath="/assets/titleImg.png"
+        btnTitle="ワールドのつくりかた"
+        btnTo="/intro"
+      >
+      <p>
+       「シングルプレイのワールドに友達を招待したい」<br />
+        「配布マップをみんなで遊びたい」<br />
+        ServerStarterを使えば新しいワールドだけでなく、<br />
+        個人ワールドや配布マップだって遊べます。
+      </p>
+      <p>簡単操作で好きなワールドをプレイしましょう！</p>
+    </ImgTxtBlock>
+  </div>
+</template>
---
+++ src/components/Pages/HOME/MultiPlayView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ImgTxtBlock from 'src/components/utils/blocks/ImgTxtBlock.vue';
+</script>
+
+<template>
+  <div class="q-pr-md">
+    <ImgTxtBlock
+      title="マルチプレイをだれでも簡単に !!"
+      imgPath="/assets/titleImg.png"
+      btnTitle="使い方を詳しく"
+      btnTo="/intro"
+    >
+      <p>
+        マインクラフトのマルチプレイで遊びたいけど、<br />
+        「やり方がわからない」<br />
+        「ポート開放ができない」<br />
+        ServerStarterはそんな人たちのためのツールです。
+      </p>
+      <p>
+        サーバーの準備からポート解放まで、<br />
+        全てServerStarterがサポートします！
+      </p>
+      <p>
+        サーバーの種類とバージョンを選んで、<br />
+        早速マルチプレイを始めましょう！
+      </p>
+    </ImgTxtBlock>
+  </div>
+</template>
