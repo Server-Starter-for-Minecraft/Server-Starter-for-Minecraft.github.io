d0101b98682030df1179ea61095354f651b38448
CivilTT <civiltt.ss@gmail.com>
2024-04-28 17:47:01 +0900

    🚑️ Fixed ImgTxtBlock's img style settings

--- src/components/utils/blocks/ImgTxtBlock.vue
+++ src/components/utils/blocks/ImgTxtBlock.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import SsImg from 'src/components/utils/base/SsImg.vue';
 import SsRouterBtn from 'src/components/utils/base/btns/ssRouterBtn.vue';
 import ssSubTitle from 'src/components/utils/base/texts/ssSubTitle.vue';
 
 interface Prop {
   title: string;
   imgPath: string;
+  imgClass?: string;
+  imgStyle?: Record<string, string>;
   btnTitle: string;
   btnTo: string;
 }
@@ -20,7 +21,15 @@ defineProps<Prop>();
     </ssSubTitle>
     <div class="row">
       <!-- 画像部分 -->
-      <SsImg :path="imgPath" class="col q-ma-md blockWidth" />
+      <div class="col blockWidth" style="height: 20rem">
+        <img
+          :src="imgPath"
+          class="fit"
+          style="object-fit: scale-down"
+          :class="imgClass"
+          :style="imgStyle"
+        />
+      </div>
 
       <!-- テキストとボタン -->
       <div class="col column q-ma-md blockWidth">
