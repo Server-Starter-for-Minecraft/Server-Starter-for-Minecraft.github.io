c3558b2b08a4014ab22427add6da0a4759c6ee85
txkodo <kodo.hsmkai@gmail.com>
2024-05-02 08:39:11 +0900

    🚚型定義を移動

---
+++ util/McModel/src/mcreource/model.ts
@@ -0,0 +1,39 @@
+export type McAxis = 'x' | 'y' | 'z';
+export type McAngle = -45 | -22.5 | 0 | 22.5 | 45;
+export type McFaceing = 'up' | 'down' | 'north' | 'south' | 'east' | 'west';
+
+export type McFaceAngle = 0 | 90 | 180 | 270;
+
+export type McPosition = [number, number, number];
+
+export type McFace = {
+  uv?: [number, number, number, number];
+  texture: string;
+  cullface?: McFaceing; // 今回は無関係
+  rotation?: McFaceAngle;
+};
+
+export type McFaces = Partial<Record<McFaceing, McFace>>;
+
+export type McRotation = {
+  origin: McPosition;
+  axis: McAxis;
+  angle: McAngle;
+  rescale?: boolean; // 処理が謎
+};
+
+export type McElement = {
+  from: McPosition;
+  to: McPosition;
+  rotation?: McRotation;
+  shade?: boolean;
+  faces: McFaces;
+};
+
+export type McTextures = Record<string, string>;
+
+export type McModel = {
+  parent?: string;
+  textures?: McTextures;
+  elements?: McElement[];
+};
