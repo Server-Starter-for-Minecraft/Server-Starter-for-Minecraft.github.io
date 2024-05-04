936c61d5d131cbc3dec5d59374a6b793993b45d8
txkodo <kodo.hsmkai@gmail.com>
2024-05-04 13:35:34 +0900

    🍱 モデルを再出力

---
+++ src/mcmodel/minecraft/mcmodels/block/brewing_stand_bottle0.ts
@@ -0,0 +1,19 @@
+import type { ModelFaces } from 'src/mcmodel/mcmodel';
+export const brewing_stand_bottle0: ModelFaces = [
+  {
+    texture: '/assets/McModel/minecraft/textures/block/brewing_stand.008g.webp',
+    matrix3d: [
+      0.0078125, 0, 0, 0, 0, 0.015625, 0, 0, 0, 0, 0.000009765625, 0, -32.25,
+      -32, 0, 1,
+    ],
+    brightness: { base: 80, amp: 30, phase: 0.25 },
+  },
+  {
+    texture: '/assets/McModel/minecraft/textures/block/brewing_stand.008g.webp',
+    matrix3d: [
+      0.0078125, 0, 0, 0, 0, 0.015625, 0, 0, 0, 0, -0.000009765625, 0, -32.25,
+      -32, -0.0006249999999999867, 1,
+    ],
+    brightness: { base: 80, amp: 30, phase: -0.25 },
+  },
+];
