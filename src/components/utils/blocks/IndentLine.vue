2b5432a5ded2e4f9fca94cff926f78722273ded6
CivilTT <civiltt.ss@gmail.com>
2024-05-06 18:16:13 +0900

    ✨ Added CreateWorldPage

--- src/components/utils/blocks/IndentLine.vue
+++ src/components/utils/blocks/IndentLine.vue
@@ -3,13 +3,20 @@ import { defineProps } from 'vue';
 
 interface Prop {
   title: string;
+  maxWidth?: string;
 }
 defineProps<Prop>();
 </script>
 
 <template>
   <div class="row q-gutter-xs">
-    <span class="col-1" style="min-width: 10rem">{{ title }}</span>
+    <span
+      class="flex"
+      style="min-width: 10rem"
+      :style="{ maxWidth: `min(${maxWidth ?? '15rem'}, 90%)` }"
+    >
+      {{ title }}
+    </span>
     <span class="col" style="min-width: 10rem"><slot /></span>
   </div>
 </template>
